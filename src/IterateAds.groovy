f17388f2b9502d2eb7be6205d24e037fa9f4b8ef
yos55 <yos.amichay5@gmail.com>
2018-02-20 00:07:44 +0200

    start work with Geb

--- src/IterateAds.groovy
+++ src/IterateAds.groovy
@@ -1,8 +1,17 @@
-@Grab(group='org.codehaus.geb', module='geb-core', version='0.7.1')
-@Grab(group='org.seleniumhq.selenium', module='selenium-chrome-driver', version='2.31.0')
-import geb.*
+
+import geb.Browser
+import org.openqa.selenium.remote.RemoteWebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-driver = {
-    System.setProperty('webdriver.chrome.driver', "C:/Program Files/WebDrivers/chromedriver.exe")
-    new ChromeDriver()
+
+class Yad_2{
+    static void main (String [] args){
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Program Files\\WebDrivers\\CHROME\\chromedriver.exe")
+        def browser = new Browser()
+        browser.driver = new ChromeDriver()
+        browser.setBaseUrl('http://www.google.com')
+        browser.go('http://m.yad2.co.il')
+
+
+    }
 }
